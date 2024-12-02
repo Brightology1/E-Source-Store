81357b2f93339d822d65c447a0312e7dd749601c
HP 250g7 <brightology2@gmain.com>
2024-12-02 17:38:53 +0100

    Login Cred from DB

---
+++ src/main/java/org/brightology/esourcestore/repository/UserRepo.java
@@ -0,0 +1,11 @@
+package org.brightology.esourcestore.repository;
+
+import org.brightology.esourcestore.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users, Integer> {
+
+    Users findByUsername(String username);
+}
