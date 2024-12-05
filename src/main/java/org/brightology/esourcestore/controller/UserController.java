fa387cd35341adb0d8e469b3997d072f4fe46736
HP 250g7 <brightology2@gmain.com>
2024-12-05 11:09:03 +0100

    BCrypt Password Encoder

---
+++ src/main/java/org/brightology/esourcestore/controller/UserController.java
@@ -0,0 +1,20 @@
+package org.brightology.esourcestore.controller;
+
+import org.brightology.esourcestore.model.Users;
+import org.brightology.esourcestore.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @PostMapping("/register")
+    public Users register(@RequestBody Users user) {
+        return service.register(user);
+    }
+}
